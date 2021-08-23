48248817729a40deace07409319a224290d16692
Aleksandar Perendic <70316883+aleksandarperendic@users.noreply.github.com>
2021-08-23 16:01:26 +0200

    Oppdaterer psvm til å skrive ut min_index.

--- src/UkeOppgaver1/UkeOppgaver1.java
+++ src/UkeOppgaver1/UkeOppgaver1.java
@@ -8,13 +8,26 @@ public class UkeOppgaver1 {
         int a[] = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
         int min_index = min(a);
 
-        System.out.println(min_index);
+        System.out.println("Tallet finnes på plass: "+min_index);
+        System.out.println("Og det er tall: "+a[min_index]);
     }
 
     static int min(int[] a){
-        int min_value = 0;
+
+        int min_value = a[0];
         int min_index = 0;
 
+        for (int i=1;i < a.length; i++){
+           int value = a[i];
+           int index = i;
+
+           //Sjekker
+           if(value < min_value) {
+               min_value = value;
+               min_index = index;
+           }
+        }
+        //Returerer index
         return min_index;
     }
 }
