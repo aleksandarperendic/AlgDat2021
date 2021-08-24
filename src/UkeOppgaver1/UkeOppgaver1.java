cd60ca65dd5e81dab48bd9edb5b2df3d43098a1f
Aleksandar Perendic <70316883+aleksandarperendic@users.noreply.github.com>
2021-08-24 14:43:23 +0200

    Oppgaver 1.1.2 - final

--- src/UkeOppgaver1/UkeOppgaver1.java
+++ src/UkeOppgaver1/UkeOppgaver1.java
@@ -2,10 +2,17 @@ package UkeOppgaver1;
 
 public class UkeOppgaver1 {
 
+    /** OPPGAVER 1.1.2
+     * 1) Metodekallet vil returnere posisjonen 6 og det er tallet: 1
+     * 2) Oppgaven besvart på intelliJ
+     * 3) Hvis største verdien kommer flere ganger så bytter man fra if(value < min_value)
+     * til if(value <= min_value).
+     */
+
     public static void main(String[] args) {
         System.out.println("Hello World - AlgDat");
 
-        int a[] = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19,1};
+        int a[] = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
         int min_index = min(a);
 
         System.out.println("Tallet finnes på plass: "+min_index);
@@ -22,7 +29,7 @@ public class UkeOppgaver1 {
            int value = a[i];
            int index = i;
            //Sjekker om det finnes mindre tall enn tall som er på a[0]
-           if(value <= min_value) { //Sjekker om den er mindre eller lik
+           if(value <= min_value) { //Sjekker om den er mindre eller lik i tilfelle at det er flere like tall
                min_value = value;
                min_index = index;
            }
