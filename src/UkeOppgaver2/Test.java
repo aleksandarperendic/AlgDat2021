42e7da2ed8441db428246408d4341c878469c510
Aleksandar Perendic <70316883+aleksandarperendic@users.noreply.github.com>
2021-09-03 14:17:55 +0200

    Øving - ukeoppgaver 2 // ferdig

--- src/UkeOppgaver2/Test.java
+++ src/UkeOppgaver2/Test.java
@@ -1,11 +1,14 @@
 package UkeOppgaver2;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        // OPPGAVE 1.2.3 - (3 og 4)
-        int[] a = {1,2,3,4,5,6,7,8,9};
-        System.out.println(Tabell.maks(a));
+        // OPPGAVE 1.2.13 - 1,2
+        int[] a = {5,8,1,3,99,31,11};
+        int[] b = Tabell.nestMaksTre(a);
+        System.out.println("M og NM er: "+Arrays.toString(b));
     }
 }
--- src/UkeOppgaver2/TestNestStorst.java
+++ src/UkeOppgaver2/TestNestStorst.java
@@ -13,20 +13,19 @@ public class TestNestStorst {
     }
 
     // SPØRRE ANDRÉ
-    public static int[] nestMaks(int[] a)
-    {
+    public static int[] nestMaks(int[] a) {
         if (a.length < 2) // må ha minst to verdier!
             throw new IllegalArgumentException("a.length(" + a.length + ") < 2!");
 
         int m = Tabell.maks(a);  // m er posisjonen til tabellens største verdi
 
-        Tabell.bytt(a,a.length-1,m);  // bytter om slik at den største kommer forrest
+        Tabell.bytt(a,0,m);  // bytter om slik at den største kommer forrest
 
-        int k = Tabell.maks(a,0,a.length-2); // a.length-1 (fasiten)
+        int k = Tabell.maks(a,1,a.length);
 
-        if (k == m) k = a.length-1; // den nest største lå opprinnelig forrest
+        if (k == m) k = 0; // den nest største lå opprinnelig forrest
 
-        Tabell.bytt(a,a.length-1,m); // bytter tilbake
+        Tabell.bytt(a,0,m); // bytter tilbake
 
         return new int[] {m,k};
 
