02867d47a8fa8e31ad4e29da7d919316d2690b20
Aleksandar Perendic <70316883+aleksandarperendic@users.noreply.github.com>
2021-09-08 11:20:20 +0200

    Oppdatering på Oppgave3.java / break som stopper for-J når den går gjennom tall

--- src/Oblig1/Oppgave3.java
+++ src/Oblig1/Oppgave3.java
@@ -2,32 +2,25 @@ package Oblig1;
 
 public class Oppgave3 {
     public static void main(String[] args) {
-        int[] a = {5,2,3,4,4,6,9,9,9,9};
+        int[] a = {1,3,2,2,2,2,2,2};
         int b = antallUlikeUsortert(a);
         System.out.println(b);
     }
 
     public static int antallUlikeUsortert(int[] a) {
-       /* int teller = 0;
-        for(int i = 0; i < a.length -1; i++) {
-            int tallsjekk = a[i];
-            for (int j = 1; j < a.length -1; j++) {
-                int tallsjekk2 = a[j];
-                if(tallsjekk != tallsjekk2){
-                    teller++;
-                    tallsjekk = a[i+1];
-                }
-            }
-        }
-        return teller; */
-
         int teller = 0;
-        int sjekker = a[0];
-        for(int i = 1; i < a.length -1; i++) {
-            if (sjekker != a[i] && a[i] != a[i+1]) {
-                teller++;
+        if (a.length > 1) {
+            teller = 1;
+            for (int i = 1; i < a.length; i++) {
+                int j;
+                for (j = 0; j < i; j++)
+                    if (a[i] == a[j]) break;
+                if (i == j)
+                    teller++;
+
             }
         }
-        return teller;
+       return teller;
+       }
     }
-}
+
